0e8be3b1eb46af51bc63f7a85060b3be8e71dc4a
Jeremy Boggs <jeremy@clioweb.org>
2012-09-24 15:21:32 -0400

    Corrects loading of Selectivizr. Reorganizes function into more logical groups.

--- custom.php
+++ custom.php
@@ -3,25 +3,25 @@
 /**
  * Adds theme assets to queues, then displays CSS and JS.
  *
+ * - Modernizr
  * - Respond.js
  * - Selectivizr
- * - Modernizr
+ * - Google Fonts style sheet.
  * - Theme style sheet.
  */
 function load_theme_assets() {
-  $respondSrc = src('respond.min.js', 'javascripts');
-  $selectivizrSrc = src('selectivizr.min.js', 'javascripts');
-
-  $modernizrLoad = "Modernizr.load([{test: Modernizr.mq(), nope: "
-    . "['$respondSrc' '$selectivizrSrc']}]);";
-
   queue_js('modernizr.min');
+
+  $respondSrc = src('respond.min.js', 'javascripts');
+  $modernizrLoad = "Modernizr.load([{test: Modernizr.mq(), nope: " . "['$respondSrc']}]);";
   queue_js_string($modernizrLoad);
 
-  queue_css('style');
+  __v()->headScript()->appendFile(src('selectivizr.min.js', 'javascripts'), 'text/javascript', array('conditional' => 'lt IE 9'));
 
   __v()->headLink()->prependStylesheet('http://fonts.googleapis.com/css?family=Bitter:400,700,400italic', 'screen');
 
-  display_css();
+  queue_css('style');
+
   display_js();
+  display_css();
 }
