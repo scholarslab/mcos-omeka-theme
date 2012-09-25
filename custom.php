bc633535d0c7167f45c926681dd6cf69f6b03392
Jeremy Boggs <jeremy@clioweb.org>
2012-09-25 16:28:49 -0400

    Moves theme functions to functions.php.
    * Renames function to queue_theme_assets for more accuracy.
    * Adds display_js and display_css back to the header partial.

--- custom.php
+++ custom.php
@@ -1,27 +1,3 @@
 <?php
 
-/**
- * Adds theme assets to queues, then displays CSS and JS.
- *
- * - Modernizr
- * - Respond.js
- * - Selectivizr
- * - Google Fonts style sheet.
- * - Theme style sheet.
- */
-function load_theme_assets() {
-  queue_js('modernizr.min');
-
-  $respondSrc = src('respond.min.js', 'javascripts');
-  $modernizrLoad = "Modernizr.load([{test: Modernizr.mq(), nope: " . "['$respondSrc']}]);";
-  queue_js_string($modernizrLoad);
-
-  __v()->headScript()->appendFile(src('selectivizr.min.js', 'javascripts'), 'text/javascript', array('conditional' => 'lt IE 9'));
-
-  __v()->headLink()->prependStylesheet('http://fonts.googleapis.com/css?family=Bitter:400,700,400italic', 'screen');
-
-  queue_css('style');
-
-  display_js();
-  display_css();
-}
+require_once dirname(__FILE__) . '/functions.php';
