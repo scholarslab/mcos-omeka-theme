a660e1870da19fbf426d0261f5c16b6ca6be743e
Jeremy Boggs <jeremy@clioweb.org>
2012-10-03 13:43:53 -0400

    Style and content updates for footer.

--- common/footer.php
+++ common/footer.php
@@ -1,10 +1,13 @@
   </div>
   <footer role="contentinfo">
-    <nav>
+    <nav class="primary">
       <ul class="navigation">
-          <?php echo public_nav_main(array(__('Home') => uri(''), __('Browse Items') => uri('items'))); ?>
+          <?php echo public_nav_main(array(__('Home') => uri(''))); ?>
+          <?php echo nav(array(__('Creative Commons') => 'http://creativecommons.org/licenses/by/2.0/', __('Contact the Project') => uri(''))); ?>
       </ul>
     </nav>
+
+    <p class="credits"><em>Catalogue of Ships</em> is a project of the <a href="http://scholarslab.org">Scholars' Lab</a> at the <a href="http://library.virginia.edu">University of Virginia Library</a>. Enean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Donec ullamcorper nulla non metus auctor fringilla.</p>
     <?php plugin_footer(); ?>
 
   </footer>
