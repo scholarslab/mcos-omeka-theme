fb90cce69fedb0c6a8e01e8a5e53112a8ffda1c1
Jeremy Boggs <jeremy@clioweb.org>
2012-12-12 15:51:16 -0500

    Displays related item thumbnails.

--- common/_item_relations.php
+++ common/_item_relations.php
@@ -4,14 +4,26 @@
     <?php if ($subjectRelations): ?>
     <ul>
     <?php foreach ($subjectRelations as $subjectRelation): ?>
-    <li><a href="<?php echo uri('items/show/' . $subjectRelation['object_item_id']); ?>"><?php echo $subjectRelation['object_item_title']; ?></a></li>
+    <li>
+        <?php $item = get_item_by_id($subjectRelation['object_item_id']); ?>
+        <?php if (item_has_thumbnail($item)) : ?>
+        <?php echo item_square_thumbnail(null, 0, $item); ?>
+        <?php endif; ?>
+        <a href="<?php echo uri('items/show/' . $subjectRelation['object_item_id']); ?>"><?php echo $subjectRelation['object_item_title']; ?></a>
+    </li>
     <?php endforeach; ?>
     </ul>
     <?php endif; ?>
     <?php if ($objectRelations): ?>
     <ul>
     <?php foreach ($objectRelations as $objectRelation): ?>
-        <li><a href="<?php echo uri('items/show/' . $objectRelation['subject_item_id']); ?>"><?php echo $objectRelation['subject_item_title']; ?></a></li>
+    <li>
+        <?php $item = get_item_by_id($objectRelation['subject_item_id']); ?>
+        <?php if (item_has_thumbnail($item)) : ?>
+        <?php echo item_square_thumbnail(null, 0, $item); ?>
+        <?php endif; ?>
+        <a href="<?php echo uri('items/show/' . $objectRelation['subject_item_id']); ?>"><?php echo $objectRelation['subject_item_title']; ?></a>
+        </li>
     <?php endforeach; ?>
     </ul>
     <?php endif; ?>
