5ff64874f30c0313f807d250514f2566872de106
David McClure <david.mcclure@virginia.edu>
2012-12-13 15:05:42 -0500

    Theme .js hack to set the starting scroll on the text container.

---
+++ neatline-editions/index/show.php
@@ -0,0 +1,39 @@
+<?php
+
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4 cc=76; */
+
+/**
+ * In-theme view.
+ *
+ * @package     omeka
+ * @subpackage  neatline
+ * @copyright   2012 Rector and Board of Visitors, University of Virginia
+ * @license     http://www.apache.org/licenses/LICENSE-2.0.html
+ */
+
+?>
+
+<?php neatline_queueNeatlineAssets(); ?>
+<?php neatline_queueEditionAssets($exhibit); ?>
+<?php neatline_queueInThemeEditionAssets(); ?>
+
+<?php
+    $head = array(
+      'bodyclass' => 'neatline primary ' . $exhibit->slug,
+      'title' => $exhibit->name);
+    head($head);
+?>
+
+<!-- The core Neatline Edition partial. -->
+<?php echo $this->partial('index/_neatlineEdition.php', array(
+    'exhibit' => $exhibit,
+    'document' => $document
+)); ?>
+
+<script>
+  // By default, scroll the text container down to
+  // frame the first cluster of highlights on the screen.
+  jQuery('.neatline-text-container').scrollTop(392);
+</script>
+
+<?php foot(); ?>
