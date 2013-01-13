35bd26836ecdb5ef5378134ba3c9bf5ebbbfbfbc
Jeremy Boggs <jeremy@clioweb.org>
2013-01-13 11:22:23 -0500

    Removes footer content.

--- common/footer.php
+++ common/footer.php
@@ -7,7 +7,6 @@
       </ul>
     </nav>
 
-    <p class="credits"><em>Catalogue of Ships</em> is a project of the <a href="http://scholarslab.org">Scholars' Lab</a> at the <a href="http://library.virginia.edu">University of Virginia Library</a>. Enean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Donec ullamcorper nulla non metus auctor fringilla.</p>
     <?php plugin_footer(); ?>
 
   </footer>
